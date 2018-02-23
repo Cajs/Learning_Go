72167626b5f2df30eb9ab592cb33e12148a4f428
Cameron Stephen <camcajs@gmail.com>
2018-02-23 22:21:56 +0000

    Add Defining Vars

---
+++ 2. Defining Vars/Defining Vars.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var testint int = 25
+	var teststring = "test"
+	var testfloat float64 = 3.1415926535
+
+	fmt.Println(testint, " ", teststring, " ", testfloat)
+}
