5ffcd5940bed2f8ac1385931f4a34b181be6aea7
Cameron Stephen <camcajs@gmail.com>
2018-02-24 19:52:34 +0000

    Defining Arrays

---
+++ 3. Defining Arrays/Defining Arrays.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers[5] int64
+
+	numbers[0] = 0
+	numbers[1] = 1
+	numbers[2] = 2
+	numbers[3] = 3
+	numbers[4] = 4
+
+	fmt.Println(numbers[3])
+}
